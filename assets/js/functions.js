cd3804fd299c9ed5f63142e6b21641456d18f789
Damian Osiecki <mlodybarney@gmail.com>
2018-08-06 13:25:17 +0200

    finnal

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,18 @@
-$( document ).ready(function() {
-
-  // Get started!
-
-});
+   $(document).ready(function() {
+         $('.icon').click(function() {
+            $('nav').toggleClass('active-toggle');
+            $('.icon').toggleClass('anime');
+            $('body').toggleClass('overly');
+         })
+         $('ul li a').click(function() {
+            $('li a').removeClass("active ");
+            $(this).addClass("active ");
+         })
+         $(window).scroll(function() {
+            if ($(window).scrollTop() >= 40) {
+               $('#head').addClass('fixed');
+            } else {
+               $('#head').removeClass('fixed');
+            }
+         });
+      });
